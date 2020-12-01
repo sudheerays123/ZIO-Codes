6cf2de47ce784ecc85a3dbf73dbb69763a69cb41
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2020-12-01 11:44:25 +0530

    Update ZIO 2020 P1.cpp

--- ZIO 2020 P1.cpp
+++ ZIO 2020 P1.cpp
@@ -1,15 +1,15 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
 
 int main() {
 	
-	ll n;
-	cin >> n;
-	
-	cout << 2*n*(n+1) + 1;
-	return 0;
+    ll n;
+    cin >> n;
+
+    cout << 2*n*(n+1) + 1;
+
+    return 0;
 }
 
 /*
